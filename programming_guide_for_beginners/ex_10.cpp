42bed149355ecb5a999f91cc4041d86670cf0a66
kiyotan815 <kyt.ibaragi0815@gmail.com>
2019-04-24 16:50:11 +0900

    11

---
+++ programming_guide_for_beginners/ex_10.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  int i = 0;
+  cout << "A:";
+  while (i < A) {
+    cout << "]";
+    i ++;
+  }
+  i = 0;
+  cout << endl << "B:";
+  while (i < B) {
+    cout << "]";
+    i ++;
+  }
+  cout << endl;
+}
